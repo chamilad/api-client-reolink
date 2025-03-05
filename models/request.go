7cf8c8aeb587ffbd1ed7d3c1f1a480486f490a8b
chamila <chamila.sujeewa@gmail.com>
2025-03-05 18:11:48 +1300

    Add initial login poc

---
+++ models/request.go
@@ -0,0 +1,36 @@
+// Package models contains the models that can be used to communciate with the camera API
+package models
+
+// RequestLogin represents the login payload to the Login action
+type RequestLogin struct {
+	Cmd    string      `json:"cmd"`
+	Action int         `json:"action"`
+	Param  interface{} `json:"param"`
+}
+
+// ParamLogin param block to be used for login requests
+type ParamLogin struct {
+	User User `json:"User"`
+}
+
+// User contains the actual credentials in the request
+type User struct {
+	UserName string `json:"userName"`
+	Password string `json:"password"`
+}
+
+// NewLoginRequest builds a login request
+func NewLoginRequest(username, password string) *[]RequestLogin {
+	return &[]RequestLogin{
+		{
+			Cmd:    "Login",
+			Action: 0,
+			Param: ParamLogin{
+				User: User{
+					UserName: username,
+					Password: password,
+				},
+			},
+		},
+	}
+}
